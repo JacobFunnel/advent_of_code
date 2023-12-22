3018362c0d669b6e5e564f96bf75f1806ea9dce7
JacobFunnel <jacob@funnel.io>
2023-12-22 22:47:51 +0100

    more

--- 2023/192.py
+++ 2023/192.py
@@ -1,18 +1,23 @@
 import re
 import operator
+from itertools import pairwise
 
 from parse import parse_lines
 from collections import namedtuple
 
 Part = namedtuple("Part", ["x", "m", "a", "s"])
 Rule = namedtuple("Rule", ["repr", "attribute", "comparison_function", "value", "destination"])
-comparison_map = {"<": operator.lt, ">": operator.gt}
+comparison_map = {
+    "<": {True: operator.lt, False: operator.ge},
+    ">": {True: operator.gt, False: operator.le},
+}
 
 
 class Node:
     def __init__(self, workflow, rules, parent, parent_trigger):
         self.workflow = workflow
         self.rules = rules
+        self.rule = rules[0]
         self.parent = parent
         self.parent_trigger = parent_trigger
         self.children = set()
@@ -28,7 +33,7 @@ class Node:
             end = ""
         else:
             end = f" {self.end_trigger}"
-        return f"{start}f {self.rules[0].repr} ({self.workflow}) {end}"
+        return f"{start}f {self.rules[0].repr} ({self.workflow}){end}"
 
 
 def parse_rule(rule):
@@ -63,21 +68,49 @@ def define_children(node):
         if rule.destination in {"A", "R"}:
             node.end = rule.destination
             node.end_trigger = True
+            # only an end node for the True path, false path can still continue
         else:
             nodes.append(Node(rule.destination, workflows[rule.destination], node, True))
             node.children.add(nodes[-1])
         # false path
-        nodes.append(Node(node.workflow, node.rules[1:], node, False))
-        node.children.add(nodes[-1])
-    else:
-        if rule.destination in {"A", "R"}:
-            node.end = rule.destination
-            node.end_trigger = False
+        if len(node.rules[1:]) == 1:
+            if node.rules[-1].destination in {"A", "R"}:
+                node.end = node.rules[-1].destination
+                node.end_trigger = False
+            else:
+                nodes.append(
+                    Node(
+                        node.rules[-1].destination,
+                        workflows[node.rules[-1].destination],
+                        node,
+                        False,
+                    )
+                )
+                node.children.add(nodes[-1])
         else:
-            nodes.append(Node(rule.destination, workflows[rule.destination], node, False))
+            nodes.append(Node(node.workflow, node.rules[1:], node, False))
             node.children.add(nodes[-1])
+
     for child in node.children:
         define_children(child)
+
+
+def create_part():
+    return Part(*(set(range(1, 4001)) for _ in range(4)))._asdict()
+
+
+def limit_attribut_values(paths):
+    valid_parts = []
+    for path in paths:
+        valid_parts.append(create_part())
+        part = valid_parts[-1]
+        for node, next_node in pairwise(path):
+            part[node.rule.attribute] = {
+                n
+                for n in part[node.rule.attribute]
+                if node.rule.comparison_function[next_node.parent_trigger](n, node.rule.value)
+            }
+    # Now what??
 
 
 workflows, parts = parse()
@@ -92,5 +125,5 @@ for node in nodes:
             next_node = paths[-1][-1]
             paths[-1].append(next_node.parent)
 
-# paths = [[node for node in path if len(node.rules) > 1 or node.end] for path in paths]
-print([list(reversed(path)) for path in paths])
+paths = [list(reversed(path)) for path in paths]
+limit_attribut_values(paths)
