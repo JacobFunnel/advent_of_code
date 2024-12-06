0e028e7468f3d864be68aa7ee01fb3bf80c2f35c
Jacob Bedro <jacob@funnel.io>
2024-12-06 16:03:34 +0100

    revert accidental refactor

--- 2021/week_4/day23part1.py
+++ 2021/week_4/day23part1.py
@@ -1,18 +1,18 @@
 with open("../input/day23_input.txt", "r") as f:
     pass
 
-visited = {"a": 0, "b": 0, "c": 0, "d": 0}
+steps = {"a": 0, "b": 0, "c": 0, "d": 0}
 cost = {"a": 1, "b": 10, "c": 100, "d": 1000}
 energy = 0
 
-visited["a"] += 2 + 3
-visited["a"] += 8 + 9
-visited["d"] += 7 + 7
-visited["c"] += 2
-visited["b"] += 6 + 4
-visited["c"] += 3 + 7
+steps["a"] += 2 + 3
+steps["a"] += 8 + 9
+steps["d"] += 7 + 7
+steps["c"] += 2
+steps["b"] += 6 + 4
+steps["c"] += 3 + 7
 
-for key, value in visited.items():
+for key, value in steps.items():
     energy += cost[key] * value
 
 print(energy)
