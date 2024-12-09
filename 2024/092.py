01e51c51b3c25a6f524c3be427e31907f1668b14
Jacob Bedro <jacob@funnel.io>
2024-12-09 22:49:27 +0100

    wip

--- 2024/092.py
+++ 2024/092.py
@@ -1,36 +1,55 @@
+from dataclasses import dataclass
+
 from parse import parse_lines
 
-disk_map = [[i, int(size), i % 2, i // 2] for i, size in enumerate(parse_lines()[0])]
+
+@dataclass
+class Block:
+    idx: int
+    size: int
+    id_: int | None
+    type_: str
+
+
+files, moved_files, empties = [], [], []
 absolute_idx = 0
-for entity in disk_map:
-    relative_idx, size, *_ = entity
-    entity[0] = absolute_idx
-    absolute_idx += size
-files  = [[idx, size, id_] for idx, size, type_, id_ in disk_map if type_ == 0]
-empties = [[idx, size, None] for idx, size, type_, id_ in disk_map if type_ == 1]
+for i, size in enumerate(parse_lines()[0]):
+    if i % 2 == 0:
+        files.append(Block(absolute_idx, int(size), i // 2, "file"))
+    else:
+        empties.append(Block(absolute_idx, int(size), None, "empty"))
+    absolute_idx += int(size)
+#
+# disk_map = [[i, int(size), i % 2, i // 2] for i, size in enumerate(parse_lines()[0])]
+# absolute_idx = 0
+# for entity in disk_map:
+#     relative_idx, size, *_ = entity
+#     entity[0] = absolute_idx
+#     absolute_idx += size
+# files  = [[idx, size, id_] for idx, size, type_, id_ in disk_map if type_ == 0]
+# empties = [[idx, size, None] for idx, size, type_, id_ in disk_map if type_ == 1]
 moved_files = []
 
 while files:
     file = files.pop()
-    file_idx, file_size, file_id = file
     for empty in empties:
-        empty_idx, empty_size, empty_id = empty
-        if empty_size >= file_size and file_idx > empty_idx:
-            file_idx = empty_idx
-            remaining_empty = empty_size - file_size
+        if empty.size >= file.size and file.idx > empty.idx:
+            file.idx = empty.idx
+            remaining_empty = empty.size - file.size
             if remaining_empty:
-                empty[0] = empty_idx + file_size
-                empty[1] = remaining_empty
+                empty.idx += file.size
+                empty.size = remaining_empty
+                empties.append(Block(file.idx + file.size, remaining_empty, None, "empty"))
             else:
                 empties.remove(empty)
             break
-    moved_files.append([file_idx, file_size, file_id])
+    moved_files.append(file)
 
-blocks = sorted(moved_files + empties, key=lambda x: x[0])
+print(sorted(moved_files + empties, key=lambda x: x.idx))
+
 check_sum = 0
-for absolute_idx, size, id_ in blocks:
-    if id_:
-        for idx in range(absolute_idx, absolute_idx + size):
-            check_sum += idx * id_
+for file in sorted(moved_files, key=lambda x: x.idx):
+        for idx in range(file.idx, file.idx + file.size):
+            check_sum += file.idx * file.id_
 
 print(check_sum)
