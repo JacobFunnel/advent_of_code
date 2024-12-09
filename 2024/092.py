d3084d9ead06d8a49a5d58f514996fbc7a4d0a5a
Jacob Bedro <jacob@funnel.io>
2024-12-09 11:29:33 +0100

    092

--- 2024/092.py
+++ 2024/092.py
@@ -1,35 +1,49 @@
 from parse import parse_lines
 
-disk_map = [[i, int(size), i % 2, i // 2] for i, size in enumerate(parse_lines()[0])]
+disk_map = [[i, int(size), ['file', 'empty'][i % 2], i // 2] for i, size in enumerate(parse_lines()[0])]
 absolute_idx = 0
 for entity in disk_map:
     relative_idx, size, *_ = entity
     entity[0] = absolute_idx
     absolute_idx += size
-files  = [[idx, size, id_] for idx, size, type_, id_ in disk_map if type_ == 0]
-empties = [[idx, size, None] for idx, size, type_, id_ in disk_map if type_ == 1]
+
+class Block:
+    def __init__(self, idx, size, id_, type_):
+        self.idx = idx
+        self.size = size
+        self.id_ = id_
+        self.type_ = type_
+
+    def get_properties(self):
+        return self.idx, self.size, self.id_, self.type_
+
+files  = [Block(idx, size, id_, type_) for idx, size, type_, id_ in disk_map if type_ == 'file']
+empties = [Block(idx, size, None, type_) for idx, size, type_, id_ in disk_map if type_ == 'empty']
 moved_files = []
+
+
 
 while files:
     file = files.pop()
-    file_idx, file_size, file_id = file
+    file_idx, file_size, file_id, _ = file.get_properties()
     for empty in empties:
-        empty_idx, empty_size, empty_id = empty
+        empty_idx, empty_size, empty_id, __ = empty.get_properties()
         if empty_size >= file_size and file_idx > empty_idx:
-            file_idx = empty_idx
+            file.idx = empty_idx
             remaining_empty = empty_size - file_size
             if remaining_empty:
-                empty[0] = empty_idx + file_size
-                empty[1] = remaining_empty
+                empty.idx = empty_idx + file_size
+                empty.size = remaining_empty
             else:
                 empties.remove(empty)
             break
-    moved_files.append([file_idx, file_size, file_id])
+    moved_files.append(file)
 
-blocks = sorted(moved_files + empties, key=lambda x: x[0])
+blocks = sorted(moved_files + empties, key=lambda x: x.idx)
 check_sum = 0
-for absolute_idx, size, id_ in blocks:
-    if id_:
+for block in blocks:
+    absolute_idx, size, id_, type_ = block.get_properties()
+    if type_ == 'file':
         for idx in range(absolute_idx, absolute_idx + size):
             check_sum += idx * id_
 
