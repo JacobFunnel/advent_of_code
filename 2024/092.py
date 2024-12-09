0c3cd7b6d60491dc7cb7cd129cc10da7cd5e7a29
Jacob Bedro <jacob@funnel.io>
2024-12-09 22:56:23 +0100

    fixed 092

--- 2024/092.py
+++ 2024/092.py
@@ -19,17 +19,8 @@ for i, size in enumerate(parse_lines()[0]):
     else:
         empties.append(Block(absolute_idx, int(size), None, "empty"))
     absolute_idx += int(size)
-#
-# disk_map = [[i, int(size), i % 2, i // 2] for i, size in enumerate(parse_lines()[0])]
-# absolute_idx = 0
-# for entity in disk_map:
-#     relative_idx, size, *_ = entity
-#     entity[0] = absolute_idx
-#     absolute_idx += size
-# files  = [[idx, size, id_] for idx, size, type_, id_ in disk_map if type_ == 0]
-# empties = [[idx, size, None] for idx, size, type_, id_ in disk_map if type_ == 1]
-moved_files = []
 
+moved_files = []
 while files:
     file = files.pop()
     for empty in empties:
@@ -39,17 +30,14 @@ while files:
             if remaining_empty:
                 empty.idx += file.size
                 empty.size = remaining_empty
-                empties.append(Block(file.idx + file.size, remaining_empty, None, "empty"))
             else:
                 empties.remove(empty)
             break
     moved_files.append(file)
 
-print(sorted(moved_files + empties, key=lambda x: x.idx))
-
 check_sum = 0
 for file in sorted(moved_files, key=lambda x: x.idx):
         for idx in range(file.idx, file.idx + file.size):
-            check_sum += file.idx * file.id_
+            check_sum += idx * file.id_
 
 print(check_sum)
