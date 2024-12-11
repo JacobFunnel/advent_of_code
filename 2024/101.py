96a900a6f6704633df2ad6974e1babef57f295f0
Jacob Bedro <jacob@funnel.io>
2024-12-11 20:41:21 +0100

    101

---
+++ 2024/101.py
@@ -0,0 +1,26 @@
+from parse import parse_lines
+
+grid = {(r, c): int(v) for r, row in enumerate(parse_lines()) for c, v in enumerate(row) if row}
+trailheads = {p: 0 for p, elevation in grid.items() if elevation == 0}
+DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+def score_trailhead(point):
+    peaks = set()
+    points = [point]
+    while points:
+        point = points.pop()
+        if grid[point] == 9:
+            peaks.add(point)
+            continue
+        points.extend(valid_neighbors(point))
+    return len(peaks)
+
+
+def valid_neighbors(point):
+    r, c = point
+    return [
+        (r + dr, c + dc) for dr, dc in DIRECTIONS if grid.get((r + dr, c + dc)) == grid[point] + 1
+    ]
+
+
+print(sum(score_trailhead(trailhead) for trailhead in trailheads))
