3f335d1cc8f3f69ff871bb564b2cb0f24ff139c3
Jacob Bedro <jacob@funnel.io>
2024-12-11 21:14:26 +0100

    111

---
+++ 2024/111.py
@@ -0,0 +1,24 @@
+import math
+
+from parse import parse_all
+
+line = [int(n) for n in parse_all().split() if n]
+
+
+def apply_rules(line):
+    next_line = []
+    for n in line:
+        if n == 0:
+            next_line.append(1)
+            continue
+        s = str(n)
+        if len(s) % 2 == 0:
+            next_line.extend([int(s[0 : len(s) // 2]), int(s[len(s) // 2 :])])
+        else:
+            next_line.append(n * 2024)
+    return next_line
+
+for _ in range(75):
+    line = apply_rules(line)
+
+print(len(line))
