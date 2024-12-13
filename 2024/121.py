fcb4a46a433f3127c32a1c0f03b77590d4f1834f
Jacob Bedro <jacob@funnel.io>
2024-12-13 22:02:04 +0100

    121

---
+++ 2024/121.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+from parse import parse_lines
+
+chars = {}
+for r, row in enumerate(parse_lines()[:-1]):
+    for c, char in enumerate(row):
+        chars.setdefault(char, set()).add(r + c * 1j)
+
+DIRECTIONS = [1, 1j, -1, -1j]
+edges = Counter()
+farms = []
+
+
+def floodfill(point):
+    global visited
+    global points
+    global edges
+    visited.add(point)
+    for direction in DIRECTIONS:
+        new_point = point + direction
+        if new_point in visited:
+            continue
+        elif new_point in points:
+            floodfill(new_point)
+        else:
+            edges[point] += 1
+
+
+for char, points in chars.items():
+    while points:
+        point = points.pop()
+        visited = set()
+        floodfill(point)
+        farms.append(visited)
+        points -= visited
+
+print(sum(len(farm) * sum(edges[point] for point in farm) for farm in farms))
